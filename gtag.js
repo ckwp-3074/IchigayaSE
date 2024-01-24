700316a1c999c050627d26012d4a51da3e857483
wp-ck <223074@cku-h.ed.jp>
2024-01-24 22:48:03 +0900

    ga

--- gtag.js
+++ gtag.js
@@ -1,5 +1,4 @@
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EVYN95Z802"></script>
 
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
